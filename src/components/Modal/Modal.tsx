11f09c94e0b14cb379da9dad753613e0e765dac0
safronman <safronmanbox@gmail.com>
2025-01-03 14:18:17 +0300

    2 portal

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,4 +1,5 @@
 import type { ReactNode } from "react"
+import { createPortal } from "react-dom"
 import s from "./Modal.module.css"
 
 type Props = {
@@ -12,16 +13,21 @@ export const Modal = ({ onClose, open, modalTitle, children }: Props) => {
   return (
     <>
       {open && (
-        <div className={s.overlay}>
-          <div className={s.content}>
-            <h3 className={s.title}>{modalTitle}</h3>
-            <hr />
-            {children}
-            <button className={s.closeIcon} onClick={onClose}>
-              x
-            </button>
-          </div>
-        </div>
+        <>
+          {createPortal(
+            <div className={s.overlay}>
+              <div className={s.content}>
+                <h3 className={s.title}>{modalTitle}</h3>
+                <hr />
+                {children}
+                <button className={s.closeIcon} onClick={onClose}>
+                  x
+                </button>
+              </div>
+            </div>,
+            document.body,
+          )}
+        </>
       )}
     </>
   )
